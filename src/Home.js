540601c8517cbb2e38d7841a1abb106c6bb77a10
Javier <javierperez@javiers-mbp.home>
2021-02-13 11:43:15 -0500

    creating a images folder in src

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,13 @@
 import {Container,Row,Col,Image} from 'react-bootstrap'
-
+import profileImg from './images/profile.jpg';
 function Home (){
-    const img = '/IMG_1950.jpg';
+    
     return(
         <Container className='main-container'> 
         <div className='container-title'><h1>About</h1></div>
             <Row >
                <Col xs={12} md={4} >
-      <Image style={{width:'200px',height:'200px'}}src={img} rounded />
+      <Image style={{width:'200px',height:'200px'}}src={profileImg} rounded />
     </Col>
                 <Col xs={12} md={8}><p>Former private pilot, with 2 years of studies in mechanical engineer.
 
